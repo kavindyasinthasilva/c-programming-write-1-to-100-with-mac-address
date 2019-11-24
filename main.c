ea5d362ff40e84ac8c03f5abc9844aa972ce8d52
kavindu yasintha silva <49961072+kavindyasinthasilva@users.noreply.github.com>
2019-11-24 12:51:36 +0530

    uploded

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+    clock_t begin = clock();
+
+    FILE *ftxtfile;
+    ftxtfile = fopen("E:\programing\c programe\sky","sky");
+
+
+    for (int i = 0; i <=100 ; ++i) {
+        fprintf(ftxtfile,"%d\n",i);
+        printf("%d\n",i);
+    }
+
+    clock_t end = clock();
+
+    float time = (float)(end-begin)/CLOCKS_PER_SEC;
+    fprintf(ftxtfile,"Running Time : %f\n",time);
+    system("getmac");
+}
